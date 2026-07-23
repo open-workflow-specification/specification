7147c1818aecc90965733e15c295336c0cdd99be
Ricardo Zanini <ricardozanini@gmail.com>
2026-07-23 15:33:35 -0400

    Address review: use node: imports and clarify template wording
    - Use node:fs and node:path import specifiers to match project convention
    - Clarify that URI templates are validated before variable expansion,
      avoiding implying strict RFC 3986 conformance for unexpanded templates
    
    Signed-off-by: Ricardo Zanini <ricardozanini@gmail.com>

--- .ci/validation/src/uri-pattern.test.ts
+++ .ci/validation/src/uri-pattern.test.ts
@@ -14,8 +14,8 @@
  * limitations under the License.
  */
 
-import * as fs from "fs";
-import * as path from "path";
+import * as fs from "node:fs";
+import * as path from "node:path";
 import * as yaml from "js-yaml";
 
 const schemaPath = path.resolve(__dirname, "../../../schema/workflow.yaml");
